14dca6232c7aad65fa2a9acede70fd42ed1fba9e
ovyas24 <ovyas24gmail.com>
2021-08-27 15:48:44 +0530

    idle test

--- server.js
+++ server.js
@@ -16,22 +16,16 @@ for (const file of commandFiles) {
 	client.commands.set(command.data.name, command);
 }
 
-let guild;
-
-const scheduledMessage = new cron.CronJob('00 29 * * * *', () => {
-	console.log(' sending -- Scheduled message');
-	const channel = guild.channels.cache.get('688735716871897145');
-	channel.send('Server activation message!!');
-});
-
 console.log('In file below fs');
 
 // eslint-disable-next-line no-shadow
-client.once('ready', (client) => {
+client.once('ready', () => {
 	console.log('Ready!');
-	guild = client.guilds.cache.get('688735716867702818');
-	scheduledMessage.start();
 });
+
+setInterval(() => {
+	console.log('wake up system removing idle!');
+}, 10 * 60 * 1000);
 
 client.on('interactionCreate', async interaction => {
 	console.log('Interaction created', interaction);
